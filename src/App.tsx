5890b407f2db342d505b1bf2d8b1f8ba0d735d83
Madiha-Shaikh <madiha.shaikh@crownstack.com>
2022-01-02 15:39:39 +0530

    modify some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Store } from './Store'
+import { IAction, IEpisode } from './interfaces'
 
-class App extends Component {
-  sum(a: number, b: number): number {
-    return a+b;
+const EpisodeList = React.lazy<any>(() => import('./EpisodesList'))
+
+export default function App(): JSX.Element {
+  const {state, dispatch} = React.useContext(Store)
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+
+  const fetchDataAction = async () => {
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-            {this.sum(5,10)}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  const toggleFavAction = (episode: IEpisode): IAction => {
+   const episodeInFav = state.favourites.includes(episode);
+   let dispatchObj = {
+    type: 'ADD_FAV',
+    payload: episode
+   }
+   if (episodeInFav) {
+     const favWithoutEpisode = state.favourites.filter((fav: IEpisode) => fav.id !== episode.id )
+    dispatchObj = {
+      type: 'REMOVE_FAV',
+      payload: favWithoutEpisode
+    }
   }
+
+   return dispatch(dispatchObj)
 }
 
-export default App;
+const props = {
+  episodes: state.episodes,
+  toggleFavAction: toggleFavAction,
+  favourites: state.favourites
+}
+
+  return (
+    <React.Fragment>
+      <header>
+        <div>
+          <h1>Rick and Morty</h1>
+          <p>Pick your favourite episode!!!</p>
+        </div>
+        <div>
+          Favourite(s): {state.favourites.length}
+        </div>
+      </header>
+      <React.Suspense fallback={<div>loading...</div>}>
+        <section className='episode-layout'>
+          <EpisodeList {...props} />
+        </section>
+      </React.Suspense>
+    </React.Fragment>
+  )
+}
